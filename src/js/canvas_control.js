482db3852f260d689e5a70c7a817bd8bf7281850
Gabriel Geraldino <gabrielgeraldinosouza@gmail.com>
2021-03-27 23:21:38 -0400

    remove console.clear()

--- src/js/canvas_control.js
+++ src/js/canvas_control.js
@@ -700,7 +700,6 @@ function OverCanvas(evt) {
 }
 
 function CanvasClick(evt) {
-    console.clear();
     // posição dos cliques descontando a distância da borda da janela
     var posX = evt.clientX - canvas.offsetLeft + window.scrollX;
     var posY = evt.clientY - canvas.offsetTop + window.scrollY;
